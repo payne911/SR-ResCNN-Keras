ea567624eb8166f955d90247fdbcf612d5d2ecc5
payne911 <jeremi.grenier.berthiaume@gmail.com>
2018-09-22 22:37:10 -0400

    test on gitignore

---
+++ crop.py
@@ -0,0 +1,26 @@
+from PIL import Image
+
+
+def crop(image_path, coords, saved_location):
+    """
+    @param image_path: The path to the image to edit
+    @param coords: A tuple of x/y coordinates (x1, y1, x2, y2)
+    @param saved_location: Path to save the cropped image
+    """
+    image_obj = Image.open(image_path)
+    cropped_image = image_obj.crop(coords)
+    cropped_image.save(saved_location)
+    cropped_image.show()
+
+
+def crop_center(img,cropx,cropy):
+    y,x = img.shape
+    startx = x//2-(cropx//2)
+    starty = y//2-(cropy//2)
+
+    return img[starty:starty+cropy, startx:startx+cropx]
+
+
+if __name__ == '__main__':
+    image = 'pictures/grasshopper.jpg'
+    crop(image, (161, 166, 706, 1050), 'cropped.jpg')
